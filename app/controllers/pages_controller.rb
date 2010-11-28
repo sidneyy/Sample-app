3320210d77dfc007527036abac76404b06920c49
Sidney <sidney.yim@gmail.com>
2010-11-29 06:51:44 +0800

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,14 @@
-git class PagesController < ApplicationController
-  def home
-  end
+class PagesController < ApplicationController
+    def home
+        @title = "Home"
+    end
 
-  def contact
-  end
+    def contact
+        @title = "Contact"
+    end
 
-  def about
-  end
+    def about
+        @title = "About"
+    end
+
 end
